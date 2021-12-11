1a98385f0157d3af1b7eedcc099ddc7e01c64a5a
John Murphy <jd031988@gmail.com>
2021-12-11 21:06:58 +0000

    Create and add new list items

--- src/script.js
+++ src/script.js
@@ -2,7 +2,41 @@ const input = document.querySelector('.my-input');
 const btn = document.querySelector('.my-btn');
 const dels = document.querySelectorAll('.del');
 const checkbox = document.querySelectorAll('.checkbox');
-const listItem = document.querySelectorAll('.todo-item')
+const listItem = document.querySelectorAll('.todo-item');
+const theList = document.querySelector('#the-list');
+
+// Create and add items to the list
+btn.addEventListener('click', function() {
+    // Check if input is empty if it is do nothing
+    if(input.value === ''){
+        return;
+    }
+    // Create list div
+    listDiv = document.createElement('div');
+    listDiv.setAttribute("class", "list-item");
+    // Create checkbox
+    ckbox = document.createElement('input');
+    ckbox.type = "checkbox";
+    ckbox.name = "checkbox";
+    ckbox.setAttribute("class", "checkbox");
+    // Create li
+    li = document.createElement('li');
+    li.setAttribute('class', 'todo-item');
+    li.innerText = input.value;
+    //  Create del btn
+    delbtn = document.createElement('span');
+    // delbtn.setAttribute('class', 'del')
+    delbtn.innerHTML = '<span class="del">&#10060</span>';
+    
+
+    
+    listDiv.appendChild(ckbox);
+    listDiv.appendChild(li);
+    listDiv.appendChild(delbtn);
+    theList.appendChild(listDiv);
+
+    input.value = '';
+})
 
 
 // Deletes selected list item
