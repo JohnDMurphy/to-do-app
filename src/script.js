9075b19e3ab86df5bc25823ab240488ac8ee99c6
John Murphy <jd031988@gmail.com>
2021-12-11 20:13:42 +0000

    when checkbox is checked cross out list item

--- src/script.js
+++ src/script.js
@@ -5,9 +5,20 @@ const checkbox = document.querySelectorAll('.checkbox');
 const listItem = document.querySelectorAll('.todo-item')
 
 
-
+// Deletes selected list item
 dels.forEach((del, i) => {
     del.addEventListener('click', function(){
         this.parentNode.remove();
+    })
+})
+
+// Checks of selected list item
+checkbox.forEach((check, i) => {
+    check.addEventListener('change', function() {
+        if(check.checked){
+            check.nextElementSibling.classList.toggle("done");
+        } else {
+            check.nextElementSibling.classList.toggle("done");
+        }
     })
 })
