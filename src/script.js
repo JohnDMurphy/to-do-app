c556631ed2e219d046ba02dd7c2055d4d9a9e49d
John Murphy <jd031988@gmail.com>
2021-12-12 14:34:04 +0000

    fix bug's to allow items to be deleted and crossed out

--- src/script.js
+++ src/script.js
@@ -1,58 +1,64 @@
-const input = document.querySelector('.my-input');
-const btn = document.querySelector('.my-btn');
-const dels = document.querySelectorAll('.del');
-const checkbox = document.querySelectorAll('.checkbox');
-const listItem = document.querySelectorAll('.todo-item');
-const theList = document.querySelector('#the-list');
+const ul = document.querySelector('#the-list');
+const inputText = document.querySelector('#my-input');
+const addBtn = document.querySelector('#my-btn');
 
-// Create and add items to the list
-btn.addEventListener('click', function() {
-    // Check if input is empty if it is do nothing
-    if(input.value === ''){
+// Creates all elements and add`s Todo
+function createTodo(){
+
+    text = inputText.value;
+
+    // If no text in the input field do nothing
+    if(text === '') {
         return;
+    };
+
+    let li = document.createElement('li');
+    li.classList.add('todo-item')
+
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+
+    let par = document.createElement('p');
+    par.textContent = text;
+    par.classList.add('para');
+
+    let delBtn = document.createElement('i')
+    delBtn.classList.add('del');
+    delBtn.innerHTML = '&cross;';
+
+    li.appendChild(checkbox);
+    li.appendChild(par);
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+
+    // Reset input field after adding todo
+    inputText.value = '';
+
+}
+
+// Deletes selected Todo
+ul.addEventListener('click', function(e) {
+    
+   if(e.target.tagName === 'I'){
+       e.target.parentElement.remove();    
+   }    
+  
+});
+
+// Toggle compleate on selected Todo
+ul.addEventListener('change', function(e) {
+    if(e.target.tagName === 'INPUT' && e.target.type === 'checkbox'){
+        e.target.nextElementSibling.classList.toggle('done');
     }
-    // Create list div
-    listDiv = document.createElement('div');
-    listDiv.setAttribute("class", "list-item");
-    // Create checkbox
-    ckbox = document.createElement('input');
-    ckbox.type = "checkbox";
-    ckbox.name = "checkbox";
-    ckbox.setAttribute("class", "checkbox");
-    // Create li
-    li = document.createElement('li');
-    li.setAttribute('class', 'todo-item');
-    li.innerText = input.value;
-    //  Create del btn
-    delbtn = document.createElement('span');
-    // delbtn.setAttribute('class', 'del')
-    delbtn.innerHTML = '<span class="del">&#10060</span>';
-    
+});
 
-    
-    listDiv.appendChild(ckbox);
-    listDiv.appendChild(li);
-    listDiv.appendChild(delbtn);
-    theList.appendChild(listDiv);
+// Add`s Todo when button is pressed
+addBtn.addEventListener('click', createTodo);
 
-    input.value = '';
-})
-
-
-// Deletes selected list item
-dels.forEach((del, i) => {
-    del.addEventListener('click', function(){
-        this.parentNode.remove();
-    })
-})
-
-// Checks of selected list item
-checkbox.forEach((check, i) => {
-    check.addEventListener('change', function() {
-        if(check.checked){
-            check.nextElementSibling.classList.toggle("done");
-        } else {
-            check.nextElementSibling.classList.toggle("done");
-        }
-    })
-})
+// Add`s Todo when enter is pressed
+inputText.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        createTodo();
+    }
+});
